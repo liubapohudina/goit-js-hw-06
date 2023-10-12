b1a820d8649665f9f30cea2cf9aaffca26d0f398
Liubov <drogaltseva92@gmail.com>
2023-10-12 21:29:27 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,14 +9,10 @@ const ingredients = [
 
 const ul = document.querySelector('#ingredients')
 for (let i = ingredients.length-1; i >= 0; i --) {
-  if (ul) {
     const firstItem = document.createElement('li');
     firstItem.textContent = ingredients[i];
     firstItem.className = 'item'
     ul.prepend(firstItem);
-  } else {
-    console.error('No matching ul element found.');
-  }
 }
 
 console.log(ul)
