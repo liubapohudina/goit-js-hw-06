646c8fe4dd5d61aa8004926a6b911d96b861c17d
Liubov <drogaltseva92@gmail.com>
2023-10-19 10:09:07 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output")
+//console.log(textInput);
+//console.log(output)
+const handInput = (event) => {
+    if (textInput.value === "") {
+        output.textContent = "Anonymous";
+    } else {
+        output.textContent = event.currentTarget.value;
+        console.log(textInput)
+    }
+};
+textInput.addEventListener("input", handInput)
+
