9da7cabb45e89c3b9e7d00a91715504362da6782
Liubov <drogaltseva92@gmail.com>
2023-10-20 19:03:27 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const currentColor = document.querySelector(".color")
+const changeColor = document.querySelector(".change-color")
+console.log(currentColor, changeColor)
+changeColor.addEventListener("click", generationColor)
+function generationColor() {
+  const color = getRandomHexColor()
+  currentColor.textContent = color
+  document.body.style.backgroundColor = color
+  changeColor.style.backgroundColor = color
+ }
