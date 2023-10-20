e6c2022ab87a3066d6cafa556d5e56575df7a445
Liubov <drogaltseva92@gmail.com>
2023-10-20 18:38:49 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const form = document.querySelector("form")
+form.addEventListener("submit", handleSubmit)
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (email.value === "" && password.value === "") {
+        const message = alert("You need to enter all fields")
+        alert(message)
+    } else {
+        const addEmail = email.value
+        const addPassword = password.value
+        console.log(addEmail, addPassword)
+    }
+    event.currentTarget.reset()
+}
