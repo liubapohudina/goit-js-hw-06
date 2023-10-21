bb9099e4f107c85ecbac58bb4b91bdc1e73f2f48
Liubov <drogaltseva92@gmail.com>
2023-10-21 19:35:19 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,8 @@
 const range = document.querySelector("#font-size-control")
 const text = document.querySelector("#text")
+ text.style.fontSize = range.value + "px";
+
+
 range.addEventListener("input", changeFontSize)
 function changeFontSize (event) {
     text.style.fontSize = event.currentTarget.value + "px";
